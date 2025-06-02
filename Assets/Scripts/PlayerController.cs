f75792e6cf99ac65a05ff0a1c171c012288c4c2e
Selman Arslanoglu <m.selman.arslanoglu@gmail.com>
2025-06-02 08:25:49 +0300

    telekineticErasure

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -171,7 +171,7 @@ public class PlayerController : MonoBehaviour
 
     private void PsiBatteryFire()
     {
-        if (Input.GetMouseButton(0))
+        if (Input.GetKeyDown(KeyCode.Mouse0))
         {
             //hangi noktadan çıkartıyorsan, o nokta vektorun hedef noktası oluyor
             reticule = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane));
